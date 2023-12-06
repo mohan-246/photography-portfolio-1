a6a122d9fa515cc008ba69a8856d69ff3ec68821
mohan-246 <mohan5497306@gmail.com>
2023-12-06 17:14:27 +0530

    finished text animations stable

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -6,100 +6,6 @@ const Landing = () => {
   gsap.registerPlugin(ScrollTrigger);
   const [curNav, setCurNav] = useState("work");
 
-  // useEffect(() => {
-  //   const track = document.getElementById("image-track");
-
-  //   gsap.to("#image-track", {
-  //     scrollTrigger: {
-  //       trigger: "body",
-  //       start: "top top",
-  //       end: "bottom bottom",
-  //       scrub: 1,
-  //       onUpdate: (self) => {
-  //         const progress = self.progress * -90;
-  //         const nextPercentage = Math.max(Math.min(progress, 0), -90);
-  //         const countpercentage = (nextPercentage / 90) * 176;
-  //         switch (true) {
-  //           case countpercentage <= 0 && countpercentage > -11:
-  //             gsap.to("#count", { y: 0, duration: 0.8, ease: Power2.easeOut });
-  //             break;
-  //           case countpercentage < -11 && countpercentage >= -33:
-  //             gsap.to("#count", {
-  //               y: -28,
-  //               duration: 0.8,
-  //               ease: Power2.easeOut,
-  //             });
-  //             break;
-  //           case countpercentage < -33 && countpercentage >= -55:
-  //             gsap.to("#count", {
-  //               y: -56,
-  //               duration: 0.8,
-  //               ease: Power2.easeOut,
-  //             });
-  //             break;
-  //           case countpercentage < -55 && countpercentage >= -77:
-  //             gsap.to("#count", {
-  //               y: -84,
-  //               duration: 0.8,
-  //               ease: Power2.easeOut,
-  //             });
-  //             break;
-  //           case countpercentage < -77 && countpercentage >= -99:
-  //             gsap.to("#count", {
-  //               y: -110,
-  //               duration: 0.8,
-  //               ease: Power2.easeOut,
-  //             });
-  //             break;
-  //           case countpercentage < -99 && countpercentage >= -121:
-  //             gsap.to("#count", {
-  //               y: -138.5,
-  //               duration: 0.8,
-  //               ease: Power2.easeOut,
-  //             });
-  //             break;
-  //           case countpercentage < -121 && countpercentage >= -143:
-  //             gsap.to("#count", {
-  //               y: -166,
-  //               duration: 0.8,
-  //               ease: Power2.easeOut,
-  //             });
-  //             break;
-  //           case countpercentage < -143 && countpercentage >= -165:
-  //             gsap.to("#count", {
-  //               y: -194,
-  //               duration: 0.8,
-  //               ease: Power2.easeOut,
-  //             });
-  //             break;
-  //           case countpercentage < -165 && countpercentage >= -176:
-  //             gsap.to("#count", {
-  //               y: -220,
-  //               duration: 0.8,
-  //               ease: Power2.easeOut,
-  //             });
-  //             break;
-  //           default:
-  //             gsap.to("#count", { y: 0, duration: 0.8, ease: Power2.easeOut });
-  //         }
-  //         track.dataset.percentage = nextPercentage;
-  //         gsap.to(track, {
-  //           xPercent: nextPercentage,
-  //           duration: 0.8,
-  //           ease: Power2.easeOut,
-  //         });
-  //         for (const image of track.getElementsByClassName("image")) {
-  //           gsap.to(image, {
-  //             objectPosition: `${100 + nextPercentage}% center`,
-  //             duration: 1,
-  //             ease: Power2.easeOut,
-  //           });
-  //         }
-  //         track.dataset.prevPercentage = nextPercentage;
-  //       },
-  //     },
-  //   });
-  // }, []);
   useEffect(() => {
     const track = document.getElementById("image-track");
     const work = document.getElementById("work");
@@ -135,56 +41,56 @@ const Landing = () => {
                 break;
               case countpercentage < -11 && countpercentage >= -33:
                 gsap.to("#count", {
-                  y: -28,
+                  y: - window.innerWidth * 0.0185,
                   duration: 0.8,
                   ease: Power2.easeOut,
                 });
                 break;
               case countpercentage < -33 && countpercentage >= -55:
                 gsap.to("#count", {
-                  y: -56,
+                  y: - window.innerWidth * 0.0360 ,
                   duration: 0.8,
                   ease: Power2.easeOut,
                 });
                 break;
               case countpercentage < -55 && countpercentage >= -77:
                 gsap.to("#count", {
-                  y: -84,
+                  y: - window.innerWidth * 0.0540,
                   duration: 0.8,
                   ease: Power2.easeOut,
                 });
                 break;
               case countpercentage < -77 && countpercentage >= -99:
                 gsap.to("#count", {
-                  y: -110,
+                  y: - window.innerWidth * 0.0720,
                   duration: 0.8,
                   ease: Power2.easeOut,
                 });
                 break;
               case countpercentage < -99 && countpercentage >= -121:
                 gsap.to("#count", {
-                  y: -138.5,
+                  y: - window.innerWidth * 0.0900,
                   duration: 0.8,
                   ease: Power2.easeOut,
                 });
                 break;
               case countpercentage < -121 && countpercentage >= -143:
                 gsap.to("#count", {
-                  y: -166,
+                  y: - window.innerWidth * 0.1080,
                   duration: 0.8,
                   ease: Power2.easeOut,
                 });
                 break;
               case countpercentage < -143 && countpercentage >= -165:
                 gsap.to("#count", {
-                  y: -194,
+                  y: - window.innerWidth * 0.1255,
                   duration: 0.8,
                   ease: Power2.easeOut,
                 });
                 break;
               case countpercentage < -165 && countpercentage >= -176:
                 gsap.to("#count", {
-                  y: -220,
+                  y: - window.innerWidth * 0.1440,
                   duration: 0.8,
                   ease: Power2.easeOut,
                 });
@@ -202,9 +108,10 @@ const Landing = () => {
               duration: 0.8,
               ease: Power2.easeOut,
             });
+            const imagePercentage = (nextPercentage / 90) * 100;
             for (const image of track.getElementsByClassName("image")) {
               gsap.to(image, {
-                objectPosition: `${100 + nextPercentage}% center`,
+                objectPosition: `${100 + imagePercentage}% center`,
                 duration: 1,
                 ease: Power2.easeOut,
               });
@@ -219,31 +126,30 @@ const Landing = () => {
       work.style.color = "gray";
       work.style.opacity = "0.75";
 
-      const aboutSection = document.getElementById("about-me");
-      const aboutChild = gsap.utils.toArray(".about-line")
-      console.log(aboutChild)
       let tl = gsap.timeline({
         scrollTrigger: {
-          trigger: aboutSection,
-          start: "top 100%",
-          end: "bottom 90%",
+          trigger: "#not-intro",
+          start: "top 85%",
+          end: "bottom 80%",
           scrub: 1,
-          markers: true,
-          onUpdate: (self) => console.log(self.progress),
         },
       });
-      
-      aboutChild.forEach((child, index) => {
+      const targets = gsap.utils.toArray(
+        "#working-experiences h2 span , #working-experiences ul li span,#awards-and-recognitions h2 span, #awards-and-recognitions ul li span ,#get-in-touch h2 span, #get-in-touch ul li span "
+      );
+      targets.forEach((child) => {
         tl.fromTo(
           child,
           {
             opacity: 0,
+            y: "4vw",
           },
           {
             opacity: 1,
-            duration: 0.6, 
-            ease : Power4.easeIn
-          },
+            duration: 0.6,
+            y: 0,
+            ease: Power4.easeIn,
+          }
         );
       });
     }
@@ -252,7 +158,9 @@ const Landing = () => {
   function showAbout() {
     const tl = gsap.timeline();
     const images = gsap.utils.toArray(".image");
-    
+    const introText = gsap.utils.toArray(
+      "#introduction  h2 span , #introduction p  span  span"
+    );
     tl.to("#plus", {
       scale: 0,
       duration: 0.6,
@@ -281,27 +189,35 @@ const Landing = () => {
         },
 
         0.4
-      ).fromTo("#about-me",{
-        
-        opacity:0
-      },
-      {
+      ).to(
+        "#about-me",
+        {
+          opacity: 1,
+        }
+      )
+      .to(introText, {
+        duration: 0.5,
+        y: 0,
         opacity: 1,
-        y:0,
-        duration: 0.5
+        ease: Power4.easeOut,
       })
   }
   function showWork() {
     const tl = gsap.timeline();
     const track = document.getElementById("image-track");
     const images = gsap.utils.toArray(".image");
-
-    tl.to(
-      "#about-me",
-      {
+    const introText = gsap.utils.toArray(
+      "#introduction  h2 span , #introduction p  span  span"
+    );
+    tl.to(introText, {
+      duration: 0.6,
+      y: "4vw",
+      opacity: 0,
+      ease: Power4.easeIn,
+    })
+      .to("#about-me", {
         opacity: 0,
-      }
-    )
+      })
       .to("#plus", {
         onStart: () => {
           scrollToTop(
@@ -343,7 +259,7 @@ const Landing = () => {
   }
   return (
     <div>
-      <div className="wrapper">
+      <section className="wrapper">
         <div
           id="image-track"
           className=""
@@ -365,7 +281,7 @@ const Landing = () => {
           <img src="/DSC_0338.jpg" alt="" className="image" draggable="false" />
           <img src="/fg 1.jpg" alt="" className="image" draggable="false" />
         </div>
-      </div>
+      </section>
       <p id="plus">+</p>
       <p id="navbar">
         <span
@@ -387,7 +303,7 @@ const Landing = () => {
           About
         </span>
       </p>
-      <div id="counter">
+      <section id="counter">
         <p id="count">
           <span id="1">1</span>
           <span id="2">2</span>
@@ -401,71 +317,89 @@ const Landing = () => {
         </p>
         <p>-</p>
         <p>9</p>
-      </div>
+      </section>
       <section id="about-me">
         <div id="introduction">
-          <h2 className="about-line">Introduction</h2>
+          <h2 className="about-line">
+            <span>Introduction</span>
+          </h2>
           <p>
             <span className="about-line">
-              Hello, I'm [Your Name], a passionate photographer dedicated{" "}
+              <span>
+                Hello, I'm [Your Name], a passionate photographer dedicated{" "}
+              </span>
             </span>
             <span className="about-line">
-              to capturing moments that tell unique stories. My journey in{" "}
+              <span>
+                to capturing moments that tell unique stories. My journey in
+              </span>
             </span>
             <span className="about-line">
-              the world of photography began [X years ago], and I've been{" "}
+              <span>
+                the world of photography began [X years ago], and I've
+              </span>
             </span>
             <span className="about-line">
-              on a mission to freeze cherished moments in time ever{" "}
+              <span>been on a mission to freeze cherished moments in time</span>
             </span>
-            <span className="about-line">since.</span>
+            <span className="about-line">
+              <span>ever since.</span>
+            </span>
           </p>
         </div>
+        <div id="not-intro">
+          <div id="working-experiences">
+            <h2 className="about-line">
+              <span>Working Experiences</span>
+            </h2>
+            <ul>
+              <li className="about-line">
+                <span>[Year]: [Job Title] at [Company/Organization] </span>
+              </li>
 
-        <div id="working-experiences">
-          <h2 className="about-line">Working Experiences</h2>
-          <ul>
-            <li>
-              <span className="about-line">
-                [Year]: [Job Title] at [Company/Organization]
-              </span>
-            </li>
-            <li>
-              <span className="about-line">
-                [Year]: [Job Title] at [Company/Organization]
-              </span>
-            </li>
-          </ul>
-        </div>
+              <li className="about-line">
+                <span>[Year]: [Job Title] at [Company/Organization] </span>
+              </li>
+            </ul>
+          </div>
 
-        <div id="awards-and-recognitions">
-          <h2 className="about-line">Awards and Recognitions</h2>
-          <ul>
-            <li>
-              <span className="about-line">[Year]: [Award Name]</span>
-            </li>
-            <li>
-              <span className="about-line">[Year]: [Award Name]</span>
-            </li>
-          </ul>
-        </div>
+          <div id="awards-and-recognitions">
+            <h2 className="about-line">
+              <span>Awards and Recognitions</span>
+            </h2>
+            <ul>
+              <li className="about-line">
+                <span>[Year]: [Award Name]</span>
+              </li>
 
-        <div id="get-in-touch">
-          <h2 className="about-line">Get in Touch</h2>
-          <ul>
-            <span className="about-line">
-              <li>Email</li>
-            </span>
-            <span className="about-line">
-              <li>LinkedIn</li>
-            </span>
-            <span className="about-line">
-              <li>Instagram</li>
-            </span>
-            <span className="about-line">
-              <li>X</li>
-            </span>
-          </ul>
+              <li className="about-line">
+                <span>[Year]: [Award Name]</span>
+              </li>
+            </ul>
+          </div>
+
+          <div id="get-in-touch">
+            <h2 className="about-line">
+              <span>Get in Touch</span>
+            </h2>
+            <ul>
+              <li className="about-line">
+                <span>Email</span>
+              </li>
+
+              <li className="about-line">
+                <span>LinkedIn</span>
+              </li>
+
+              <li className="about-line">
+                <span>Instagram</span>
+              </li>
+
+              <li className="about-line">
+                <span>X</span>
+              </li>
+            </ul>
+          </div>
         </div>
       </section>
     </div>
