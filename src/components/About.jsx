800030c079b312090e79775944e622b840f3bc08
mohan-246 <mohan5497306@gmail.com>
2023-12-24 15:51:38 +0530

    template final

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,15 @@
 /* eslint-disable react/prop-types */
 const About = ({
   mailtoLink,
-  about1Paragraph,
+  about1Highlight,
+  about1ParagraphBefore,
+  about1ParagraphAfter,
   about3Paragraph,
   about1ImageSource,
   about2ImageSource2,
-  about2HeadingText,
+  about2HeadingTextBefore,
+  about2HeadingTextAfter,
+  about2HeadingHighlight,
   about2Paragraph,
   linkedInLink,
   instagramLink,
@@ -30,7 +34,11 @@ const About = ({
               <path d="M512 255.1c0 11.34-7.406 20.86-18.44 23.64l-171.3 42.78l-42.78 171.1C276.7 504.6 267.2 512 255.9 512s-20.84-7.406-23.62-18.44l-42.66-171.2L18.47 279.6C7.406 276.8 0 267.3 0 255.1c0-11.34 7.406-20.83 18.44-23.61l171.2-42.78l42.78-171.1C235.2 7.406 244.7 0 256 0s20.84 7.406 23.62 18.44l42.78 171.2l171.2 42.78C504.6 235.2 512 244.6 512 255.1z" />
             </svg>
           </span>
-          <p>{about1Paragraph}</p>
+          <p>
+            {about1ParagraphBefore}
+            <span className=".color-text">{about1Highlight}</span>{" "}
+            {about1ParagraphAfter}
+          </p>
         </div>
         <div id="about-pic">
           <img id="photographer" src={about1ImageSource} alt="" />
@@ -41,7 +49,11 @@ const About = ({
           <img id="photographer-2" src={about2ImageSource2} alt="" />
         </div>
         <div id="intro-2">
-          <h2>{about2HeadingText}</h2>
+          <h2>
+            {about2HeadingTextBefore}{" "}
+            <span className=".color-text">{about2HeadingHighlight}</span>{" "}
+            {about2HeadingTextAfter}{" "}
+          </h2>
           <span className="magic-star">
             <svg viewBox="0 0 512 512">
               <path d="M512 255.1c0 11.34-7.406 20.86-18.44 23.64l-171.3 42.78l-42.78 171.1C276.7 504.6 267.2 512 255.9 512s-20.84-7.406-23.62-18.44l-42.66-171.2L18.47 279.6C7.406 276.8 0 267.3 0 255.1c0-11.34 7.406-20.83 18.44-23.61l171.2-42.78l42.78-171.1C235.2 7.406 244.7 0 256 0s20.84 7.406 23.62 18.44l42.78 171.2l171.2 42.78C504.6 235.2 512 244.6 512 255.1z" />
